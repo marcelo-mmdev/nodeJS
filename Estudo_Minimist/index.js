e2fb7a55218c5c363912476a3fb17be803eb969f
Marcelo Rodrigues <manoelmarcelo.r@gmail.com>
2023-05-17 10:22:49 -0300

    organiza

---
+++ Estudo_Minimist/index.js
@@ -0,0 +1,9 @@
+const minimist = require("minimist");
+const args = minimist(process.argv.slice(2));
+
+console.log(args);
+
+const nome = args["nome"];
+//console.log(nome);
+const idade = args["idade"];
+console.log(`Meu nome é ${nome} e minha idade é ${idade}`);
---
+++ Sistema_Banco/index.js
@@ -0,0 +1,8 @@
+// modules externos
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+// modules internos
+const fs = require("fs");
+
+console.log("Hello World");
